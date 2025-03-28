690e267548950c3a34cede7f4e692841b6f2f9fc
NayronCampos <nayroncampossoares@gmail.com>
2025-03-28 20:54:49 -0300

    Update ContentView.swift

--- Desafio4/Desafio4/ContentView.swift
+++ Desafio4/Desafio4/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var valor2: Int = 0
     @State private var calculo: Int = 0
     @State private var corfund: Color = Color(.n1)
-    @State private var imge: String = "null"
+    @State private var imge: String = ""
    
 
     var body: some View {
@@ -53,31 +53,29 @@ struct ContentView: View {
                          calculo = valor1 + valor2
                         
                         
-                        if(calculo>=0 || calculo<=9){
+                        if(calculo>=0 && calculo<=9){
                             corfund = .n1
-                            imge = ".img1"
+                            imge = "img1"
                         }
-                        else if(calculo>=10 || calculo<=20){
+                        else if(calculo>=10 && calculo<=20){
                             corfund = .n2
-                            imge = ".im2"
+                            imge = "im2"
                         }
-                        else if(calculo>=21 || calculo<=30){
-                            corfund = .n2
-                            imge = ".im2"
+                        else if(calculo>=21 && calculo<=30){
+                            corfund = .n3
+                            imge = "img3"
                         }
-                        else if(calculo>=31 || calculo<=40){
-                            corfund = .n2
-                            imge = ".im2"
+                        else if(calculo>=31 && calculo<=40){
+                            corfund = .n4
+                            imge = "img4"
                         }
-                        else if(calculo>=41 || calculo<=50){
-                            corfund = .n2
-                            imge = ".im2"
+                        else if(calculo>=41 && calculo<=50){
+                            corfund = .n5
+                            imge = "img5"
                         }
                         
                         
                     }
-                    
-                    if(calculo==0){
                     
                         Spacer()
                         
@@ -86,59 +84,13 @@ struct ContentView: View {
                                 .frame(width: 10, height: 10)
                             
                             VStack{
-                                Image("img1")
+                                Image(imge)
                                     .resizable()
+                                    .frame(width: 200, height: 200)
                                     .scaledToFit()
                                     .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                                     .padding()
                             }
-                            
-                        }
-                    
-                    
-                    else if(calculo==1){
-                        Text("Its a: \(calculo) ")
-                        Spacer()
-                        
-                            .frame(width: 10, height: 10)
-                        
-                        VStack{
-                            Image("im2")
-                                .resizable()
-                                .scaledToFit()
-                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                                .padding()
-                        }
-                    }
-                    else if(calculo==2){
-                        Text("Its a: \(calculo) ")
-                        Spacer()
-                        
-                            .frame(width: 10, height: 10)
-                        
-                        VStack{
-                            Image("im2")
-                                .resizable()
-                                .scaledToFit()
-                                .clipShape(Circle())
-                                .padding()
-                        }
-                    }
-                    else if(calculo==3){
-                        Text("Its a: \(calculo) ")
-                        Spacer()
-                        
-                            .frame(width: 10, height: 10)
-                        
-                        VStack{
-                            Image("im2")
-                                .resizable()
-                                .scaledToFit()
-                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                                .padding()
-                        }
-                    }
-                    
                     VStack{
                         
                         Text("Tempo médio dos developers:\n P1: ( 00:00 minutes)\n P2: ( 00:00 minutes)\n P3: ( 00:00 minutes)\n P4: ( 00:00 minutes)\n P5: ( 00:00 minutes)")
