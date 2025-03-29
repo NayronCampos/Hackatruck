ac7c568df9a4dc84fa38784365d87f807514ea48
NayronCampos <nayroncampossoares@gmail.com>
2025-03-29 15:51:42 -0300

    Add desing

--- Desafio7/SecondView.swift
+++ Desafio7/SecondView.swift
@@ -8,42 +8,83 @@
 import SwiftUI
 
 struct SecondView: View {
-    @State private var name: String = ""
+    
+    @State var musica : Song
+   // @State var musica2 : sugs
+    
     var body: some View {
-        ZStack{
-            Color(.darkGray)
-                .ignoresSafeArea()
-            
+        
+        ZStack {
+                Image("back_green")
+                    .resizable()
+                    .ignoresSafeArea()
+           
+            VStack {
+                Spacer()
+                    .frame(height: 200)
+                Text("Playlist bom")
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .bottom)
+                    .font(.system(
+                        size: 25,
+                        weight: .heavy
+                    ))
                 
-                
-                VStack{
-                    Spacer()
-                        .frame(height: 0)
-                            Text("Modo 1")
-                        .font(.title)
-                            Spacer()
-                        .frame(height: 100)
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 9.0)
-                            .foregroundColor(.pink)
-                            .frame(width: 250, height: 140)
-                            
-                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                            TextField("your name",text: $name)
-                                .multilineTextAlignment(.center)
-                        Spacer()
-                            .frame(height: 100)
-                            
-                        
+                VStack {
+                    AsyncImage (url: URL(string: musica.capa)) { image in
+                        image.resizable()
+                    } placeholder: {
+                        ProgressView()
                     }
                     
+                    .frame(width: 350, height: 350)
+                    .padding()
+    
                 }
-            
-            
+                VStack{
+                    
+                    Text(musica.name)
+                    
+                }
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .font(.system(size: 28, weight: .heavy))
+               
+                VStack{
+                    
+                    Text(musica.artist)
+                    
+                }
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .font(.system(size: 20))
+               
+                Text("_____")
+                    .font(.system(size: 100))
+                    .foregroundColor(.white)
+                
+                HStack{
+                    
+                    Image(systemName: "arrowtriangle.backward")
+                    Text("  ")
+                    Image(systemName: "play.fill")
+                    Text("  ")
+                    Image(systemName: "arrowtriangle.right")
+                    
+                }
+                .font(.system(size: 50))
+                .foregroundColor(.white)
+                Spacer()
+                    .frame(height: 290)
+                
+            }
+           
         }
+        
+        
+
     }
 }
-
 
 #Preview {
     ContentView()
