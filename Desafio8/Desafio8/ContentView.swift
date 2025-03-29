c51d2da529de5afa81a5870f72598a6022559962
NayronCampos <nayroncampossoares@gmail.com>
2025-03-29 15:50:19 -0300

    Atualização com sheet view e onChange

--- Desafio8/Desafio8/ContentView.swift
+++ Desafio8/Desafio8/ContentView.swift
@@ -1,20 +1,12 @@
-//
-//  ContentView.swift
-//  Desafio8
-//
-//  Created by Turma02-25 on 25/03/25.
-//
-
 import SwiftUI
 import MapKit
 
-struct Location: Hashable{
+struct Location: Hashable {
     let nome: String
     let foto: String
     let descricao: String
     let latitude: Double
     let longitude: Double
-   
 }
 
 struct ContentView: View {
@@ -22,37 +14,30 @@ struct ContentView: View {
         MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: -22.90682, longitude: -43.1729), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
     
-    @State var loc =   Location( nome: "Rio de Janeiro", foto: "https://www.google.com/search?q=rio+de+janeiro+imagem&tbm=isch", descricao: "Famosa por suas belas praias, o Cristo Redentor e o Pão de Açúcar.",latitude: -22.90682, longitude: -43.1729)
+    @State private var selectedLocation = Location(nome: "Rio de Janeiro", foto: "", descricao: "", latitude: -22.90682, longitude: -43.1729)
     
-    let (arrayLocation) = [
-        Location( nome: "Rio de Janeiro", foto: "https://www.google.com/search?q=rio+de+janeiro+imagem&tbm=isch", descricao: "Famosa por suas belas praias, o Cristo Redentor e o Pão de Açúcar.",latitude: -22.90682, longitude: -43.1729),
-        
-        Location( nome: "São Paulo", foto: "https://www.google.com/search?q=sao+paulo+imagem&tbm=isch", descricao: "Maior cidade do Brasil, conhecida pela sua cultura, gastronomia e negócios.", latitude:-23.5505, longitude: -46.6333),
-                 
-        Location( nome: "Salvador", foto: "https://www.google.com/search?q=salvador+imagem&tbm=isch", descricao: "Famosa pelo carnaval, pelas suas praias e pelo Pelourinho, centro histórico da cidade.", latitude:-12.9714, longitude:-38.5014),
-        
-        Location( nome: "Florianópolis", foto: "https://www.google.com/search?q=florianopolis+imagem&tbm=isch", descricao: "Cidade litorânea conhecida por suas belas praias e como destino turístico popular.", latitude:-27.5954, longitude: -48.5480),
-        
-        Location( nome: "Curitiba", foto: "https://www.google.com/search?q=curitiba+imagem&tbm=isch", descricao: "Famosa por seu planejamento urbano, parques e clima frio.",latitude:-25.4296, longitude: -49.2711),
-        
-        Location( nome: "Manaus", foto: "https://www.google.com/search?q=manaus+imagem&tbm=isch", descricao: "Capital da Amazônia, famosa pela floresta tropical e o Encontro das Águas.",latitude:-3.1190, longitude: -60.2449),
-        
-        Location( nome: "Fortaleza", foto: "https://www.google.com/search?q=fortaleza+imagem&tbm=isch", descricao: "Cidade do nordeste, famosa pelas suas praias e pelo vibrante clima carnavalesco.",latitude:-3.7172, longitude: -38.5437),
-        
-            Location( nome: "Recife", foto: "https://www.google.com/search?q=recife+imagem&tbm=isch", descricao: "Conhecida por seu carnaval, suas praias e o bairro histórico do Recife Antigo.",latitude:-8.0476, longitude: -34.8770)
-        ]
-
+    let arrayLocation = [
+        Location(nome: "Rio de Janeiro", foto: "https://wamcomercializacao.com/wp-content/uploads/2022/07/Banner_1-1024x680.jpg", descricao: "Famosa por suas belas praias, o Cristo Redentor e o Pão de Açúcar.", latitude: -22.90682, longitude: -43.1729),
+        Location(nome: "São Paulo", foto: "https://www.zapimoveis.com.br/blog/wp-content/uploads/2023/12/cidade-de-sao-paulo.jpg", descricao: "Maior cidade do Brasil, conhecida pela sua cultura, gastronomia e negócios.", latitude: -23.5505, longitude: -46.6333),
+        Location(nome: "Salvador", foto: "https://cdn.ibahia.com/img/Facebook/320000/1210x720/Voce-conhece-Salvador-Mapa-mostra-onde-comeca-e-te0032360100202408050941-3.webp?fallback=https%3A%2F%2Fcdn.ibahia.com%2Fimg%2FFacebook%2F320000%2FVoce-conhece-Salvador-Mapa-mostra-onde-comeca-e-te0032360100202408050941.jpg%3Fxid%3D1643923&xid=1643923", descricao: "Famosa pelo carnaval, pelas suas praias e pelo Pelourinho, centro histórico da cidade.", latitude: -12.9714, longitude: -38.5014),
+        Location(nome: "Florianópolis", foto: "https://static.ndmais.com.br/2023/03/florianopolis-aerea-scaled.jpg", descricao: "Cidade litorânea conhecida por suas belas praias e como destino turístico popular.", latitude: -27.5954, longitude: -48.5480),
+        Location(nome: "Curitiba", foto: "https://upload.wikimedia.org/wikipedia/commons/1/19/Jardim_Bot%C3%A2nico_Centro_Curitiba.jpg", descricao: "Famosa por seu planejamento urbano, parques e clima frio.", latitude: -25.4296, longitude: -49.2711),
+        Location(nome: "Manaus", foto: "https://upload.wikimedia.org/wikipedia/commons/7/76/Praia_da_Ponta_Negra_%28Manaus%29.jpg", descricao: "Capital da Amazônia, famosa pela floresta tropical e o Encontro das Águas.", latitude: -3.1190, longitude: -60.2449),
+        Location(nome: "Fortaleza", foto: "https://viagemeturismo.abril.com.br/wp-content/uploads/2016/12/praia-de-meireles-e1482425853260.jpg", descricao: "Cidade do nordeste, famosa pelas suas praias e pelo vibrante clima carnavalesco.", latitude: -3.7172, longitude: -38.5437),
+        Location(nome: "Recife", foto: "https://www.viagensecaminhos.com/wp-content/uploads/2014/05/recife-pe.jpg", descricao: "Conhecida por seu carnaval, suas praias e o bairro histórico do Recife Antigo.", latitude: -8.0476, longitude: -34.8770)
+    ]
     
+    @State private var showingSheet = false
     
     var body: some View {
-        ZStack{
+        ZStack {
             Map(position: $position) {
                 ForEach(arrayLocation, id: \.self) { location in
-                    Annotation(location.nome, coordinate:  CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
-                        
-                        Button{
-                            
-                        }label: {
+                    Annotation(location.nome, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
+                        Button {
+                            selectedLocation = location
+                            showingSheet = true
+                        } label: {
                             Image(systemName: "mappin.and.ellipse.circle.fill")
                                 .resizable()
                                 .frame(width: 50, height: 50)
@@ -61,33 +46,71 @@ struct ContentView: View {
                 }
             }
             .ignoresSafeArea()
-            VStack{
-                
-                
-                ZStack{
+            
+            .sheet(isPresented: $showingSheet) {
+                LocationDetailView(location: $selectedLocation)
+            }
+            
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .frame(width: 150, height: 50)
+                        .foregroundColor(.white)
+                        .opacity(0.6)
                     
-                    Picker(loc.nome, selection: $loc ){
+                    Picker(selectedLocation.nome, selection: $selectedLocation) {
                         ForEach(arrayLocation, id: \.self) { location in
                             Text(location.nome)
                         }
-                        
-                    }//onChange{mudar o position}
-                    
+                    }
+                    .onChange(of: selectedLocation) { newLocation in
+                        // Atualizando a posição do mapa ao selecionar a cidade no Picker
+                        position = MapCameraPosition.region(
+                            MKCoordinateRegion(
+                                center: CLLocationCoordinate2D(latitude: newLocation.latitude, longitude: newLocation.longitude),
+                                span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                            )
+                        )
+                    }
                 }
-                Spacer()
-                    .frame(height: 620)
-                ZStack{
-                    
+                
+                Spacer().frame(height: 620)
+                
+                ZStack {
                     RoundedRectangle(cornerRadius: 9.0)
-                        .foregroundColor(.gray)
+                        .foregroundColor(.white)
+                        .opacity(0.4)
                         .frame(width: 250, height: 60)
-                        Text("Maravilhas do mundo moderno")
-                    
+                    Text("Maravilhas do mundo moderno")
                 }
             } // Fim VStack
         } // Fim ZStack
-        
-        
+    }
+}
+
+struct LocationDetailView: View {
+    @Binding var location: Location
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            
+            AsyncImage(url: URL(string: location.foto)){ image in
+                image
+                    .resizable()
+                    .frame(width: 300, height: 300)
+            }
+        placeholder: {
+            ProgressView()
+        }
+            Text(location.nome)
+                .font(.title)
+                .padding()
+            
+            Text(location.descricao)
+                .padding()
+            
+            
+        }
     }
 }
 
